1efe5a275b82197e70dd980eb8f5bbf0ecf3c10c
jhimli <jhimli@1osolutions.com>
2020-11-06 12:34:34 +0530

    Initial Commit

---
+++ NetworkUtility/NetworkUtility.swift
@@ -0,0 +1,11 @@
+//
+//  NetworkUtility.swift
+//  NetworkUtility
+//
+//  Created by Jhimlione1 on 06/11/20.
+//  Copyright © 2020 One O Tech Labs. All rights reserved.
+//
+
+class NetworkUtility {
+
+}
